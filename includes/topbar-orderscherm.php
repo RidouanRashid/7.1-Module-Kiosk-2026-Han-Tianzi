4c5bde29001317181d6151125cce72c0b13a2089
NoaPhilene <philenevanmil@gmail.com>
2026-02-09 16:32:58 +0100

    begin topbar

---
+++ includes/topbar-orderscherm.php
@@ -0,0 +1,5 @@
+<div id="achtergrond-topbar">
+    <img src="assets\logo\logo_big_complete_transparent.webp" id="Happy-Herbivore-logo" alt="Happy-Herbivore-logo">
+    <img src="assets\logo\logo_big_happy_herbivore_transparent.webp" id="Happy-Herbivore-Tekst" alt="Happy-Herbivore-Tekst">
+    <img src="assets\pagina-deco\winkelmandje.png" id="winkelmandje" alt="winkelmandje">
+</div>
