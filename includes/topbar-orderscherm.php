91c94eacd0a50d8afa29e3ebe0354ca5fe15dd5a
NoaPhilene <philenevanmil@gmail.com>
2026-03-03 00:00:59 +0100

    sidebar gefixed

--- includes/topbar-orderscherm.php
+++ includes/topbar-orderscherm.php
@@ -1,5 +1,7 @@
 <div id="achtergrond-topbar">
-    <img src="assets\logo\logo_big_complete_transparent.webp" id="Happy-Herbivore-logo" alt="Happy-Herbivore-logo">
+    <a href="kies-order-begin.php">
+        <img src="assets\logo\logo_big_complete_transparent.webp" id="Happy-Herbivore-logo" alt="Happy-Herbivore-logo">
+    </a>
     <img src="assets\logo\logo_big_happy_herbivore_transparent.png" id="Happy-Herbivore-Tekst" alt="Happy-Herbivore-Tekst">
     <img src="assets\pagina-deco\winkelmandje.png" id="winkelmandje" alt="winkelmandje">
 </div>
