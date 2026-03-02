25d893737be3d076b1b8a0494158763165dcfb03
NoaPhilene <philenevanmil@gmail.com>
2026-03-02 22:53:46 +0100

    het switchen tussen catogorien werkent + producten worden weer gegeven

--- includes/sidebar-orderscherm.php
+++ includes/sidebar-orderscherm.php
@@ -5,36 +5,46 @@
         <p>Menu</p>
     </div>
 
-    <a href="kies-orders.php">
+    <a href="kies-orders.php?cat=1">
         <div class="catogorie">
             <img src="assets/pagina-deco/icoontjes/breakfast-removebg-preview.png" id="breakfast-icoon"><br>
             <p>Breakfast</p>
         </div>
     </a>
 
-    <div class="catogorie">
-        <img src="assets/pagina-deco/icoontjes/lunch-removebg-preview.png" id="lunch-icoon"><br>
-        <p>Lunch & Dinner</p>
-    </div>
+    <a href="kies-orders.php?cat=2">
+        <div class="catogorie">
+            <img src="assets/pagina-deco/icoontjes/lunch-removebg-preview.png" id="lunch-icoon"><br>
+            <p>Lunch & Dinner</p>
+        </div>
+    </a>
 
-    <div class="catogorie">
-        <img src="assets/pagina-deco/icoontjes/handhelds-removebg-preview.png" id="handhelds-icoon"><br>
-        <p>Handhelds</p>
-    </div>
+    <a href="kies-orders.php?cat=3">
+        <div class="catogorie">
+            <img src="assets/pagina-deco/icoontjes/handhelds-removebg-preview.png" id="handhelds-icoon"><br>
+            <p>Handhelds</p>
+        </div>
+    </a>
 
-    <div class="catogorie">
-        <img src="assets/pagina-deco/icoontjes/sides-removebg-preview.png" id="sides-icoon"><br>
-        <p>Sides</p>
-    </div>
+    <a href="kies-orders.php?cat=4">
+        <div class="catogorie">
+            <img src="assets/pagina-deco/icoontjes/sides-removebg-preview.png" id="sides-icoon"><br>
+            <p>Sides</p>
+        </div>
+    </a>
 
-    <div class="catogorie">
-        <img src="assets/pagina-deco/icoontjes/drinks-removebg-preview.png" id="drinks-icoon"><br>
-        <p>Drinks</p>
-    </div>
+    <a href="kies-orders.php?cat=5">
+        <div class="catogorie">
+            <img src="assets/pagina-deco/icoontjes/drinks-removebg-preview.png" id="drinks-icoon"><br>
+            <p>Drinks</p>
+        </div>
+    </a>
 
-    <div class="catogorie">
-        <img src="assets/pagina-deco/icoontjes/dips-removebg-preview.png" id="dips-icoon"><br>
-        <p>Dips</p>
-    </div>
+    <a href="kies-orders.php?cat=6">
+        <div class="catogorie">
+            <img src="assets/pagina-deco/icoontjes/dips-removebg-preview.png" id="dips-icoon"><br>
+            <p>Dips</p>
+        </div>
+    </a>
 
 </div>
